e229cdf19ddf490d100a30c0c30b9aa08aecddf1
LuoHaoAt2017 <hluo5206@gmail.com>
2022-12-06 21:55:22 +0800

    task when all and any

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,58 @@
-﻿public class Program
-{
-	public static void Main(string[] args)
+﻿{
+	internal class Program
 	{
-		Console.WriteLine("Hello World");
+		static void Main(string[] args)
+		{
+			// TestTaskWhenAll();
+			TestTaskWhenAny();
+			Console.ReadLine();
+		}
+
+		public static int TestTaskMethod(int second)
+		{
+			Thread.Sleep(TimeSpan.FromSeconds(second));
+			return second;
+		}
+
+		public static void TestTaskWhenAll()
+		{
+			Task<int> task1 = new Task<int>(() => TestTaskMethod(1));
+			Task<int> task2 = new Task<int>(() => TestTaskMethod(2));
+			task1.Start();
+			task2.Start();
+			Console.WriteLine("1");
+
+			Task.WhenAll(task1, task2).ContinueWith((t) =>
+			{
+				Console.WriteLine("2");
+
+				if (t.Status == TaskStatus.RanToCompletion)
+				{
+					foreach(var result in t.Result)
+					{
+						Console.WriteLine($"result: {result}");
+					}
+				}
+
+				if (t.Status == TaskStatus.Faulted)
+				{
+
+				}
+			});
+
+			Console.WriteLine("3");
+		}
+
+		public static void TestTaskWhenAny()
+		{
+			Task<int> task1 = new Task<int>(() => TestTaskMethod(1));
+			Task<int> task2 = new Task<int>(() => TestTaskMethod(2));
+			task1.Start();
+			task2.Start();
+			Task.WhenAny(task1, task2).ContinueWith((t) =>
+			{
+				Console.WriteLine($"completed: {t.Result.Result}");
+			});
+		}
 	}
 }
